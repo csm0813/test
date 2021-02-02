39cdc8e52c2a1ee7018ff4c6026f7459f10fed90
csm0813 <1224984356@qq.com>
2021-02-02 21:36:15 +0800

    创建login视图

--- index.py
+++ index.py
@@ -4,6 +4,10 @@ def index(request):
     return django.Http.HttpResponse("index.html")
 
 
+def login(request):
+    return django.Http.HttpResponse("login.html")
+
+
 if __name__ == "__main__":
     main()
 
